f2ecff927c2ba69366a7f5275978ca882facbc8e
eminoda <245978782@qq.com>
2020-03-30 20:52:19 +0800

    feat #antd mobile 支持

--- config/merge/webpack.module.js
+++ config/merge/webpack.module.js
@@ -8,8 +8,21 @@ module.exports = {
         include: util.resolveApp('src'),
         loader: require.resolve('babel-loader'),
         options: {
-          plugins: ['@babel/plugin-transform-react-jsx']
+          plugins: [
+            '@babel/plugin-transform-react-jsx',
+            ['import', { libraryName: 'antd-mobile', style: 'css' }] // `style: true` 会加载 less 文件
+          ]
         }
+      },
+      /** css */
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader'
+        ]
       },
       /** sass */
       {
--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 require('./style.scss');
 import React from 'react';
+import { Button, List } from 'antd-mobile';
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <div>React Hello</div>;
+    return (
+      <div>
+        React Hello
+        <Button type='ghost' size='small' inline>
+          small
+        </Button>
+      </div>
+    );
   }
 }
 export default App;
